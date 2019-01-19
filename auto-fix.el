e21ffd44258a8a3217c3a9dd7e9bc04e069115b9
tomoya <tomoya.ton@gmail.com>
2019-01-20 01:24:47 +0900

    Add setup comment

--- auto-fix.el
+++ auto-fix.el
@@ -34,6 +34,20 @@
 ;; This package is a minor mode to fix current buffer using linter
 ;; and etc.
 ;;
+;; Setup
+;; -----
+;;
+;; To enable auto-fix before saving add the following to your init file:
+;;
+;;    (defun setup-ts-auto-fix ()
+;;      (setq-local auto-fix-command "tslint")
+;;      (auto-fix-mode +1))
+;;
+;;    (add-hook 'auto-fix-mode-hook
+;;              (lambda () (add-hook 'before-save-hook #'auto-fix-before-save)))
+;;
+;;    (add-hook 'typescript-mode-hook #'setup-ts-auto-fix)
+;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; code:
