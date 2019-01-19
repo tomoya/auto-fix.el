a09764d0faef511048659d860fe65e26c86bae0d
tomoya <tomoya.ton@gmail.com>
2019-01-20 00:49:12 +0900

    Initial commit

---
+++ auto-fix.el
@@ -0,0 +1,46 @@
+;;; auto-fix.el --- minor mode to fix current buffer using linter and etc -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 tomoya.
+
+;; Author: Tomoya Otake <tomoya.ton@gmail.com>
+;; Maintainer: Tomoya Otake <tomoya.ton@gmail.com>
+;; Keywords: linter, languages, tools
+;; Version: 1.0.0
+;; X-URL: https://github.com/tomoya/auto-fix/
+;; Package-Requires: ((emacs "26.1"))
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Introduction
+;; ------------
+;;
+;; This package is a minor mode to fix current buffer using linter
+;; and etc.
+;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; code:
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide 'auto-fix)
+
+;;; auto-fix.el ends here
