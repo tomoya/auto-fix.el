5d20918c8f125a2cf8b8ce15b141fb8685b9613e
tomoya <tomoya.ton@gmail.com>
2019-01-20 00:58:28 +0900

    Define command and functions

--- auto-fix.el
+++ auto-fix.el
@@ -38,6 +38,8 @@
 
 ;;; code:
 
+(require 'cl-lib)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; User Variables:
@@ -65,6 +67,145 @@ a `before-save-hook'."
 (defvar-local auto-fix-option "--fix"
   "Set fix option string.
 Default is `--fix`")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; User commands
+
+(defun auto-fix ()
+  "Format the current buffer according to the formatting tool.
+The tool used can be set via ‘auto-fix-command` and additional
+arguments can be set as a list via ‘auto-fix-option`."
+  (interactive)
+  (let ((tmpfile (auto-fix--make-temp-file))
+        (patchbuf (get-buffer-create "*Auto-fix patch*"))
+        (errbuf (if auto-fix-show-errors (get-buffer-create "*Auto-fix Errors*")))
+        (coding-system-for-read 'utf-8)
+        (coding-system-for-write 'utf-8)
+        our-auto-fix-args)
+
+    (unwind-protect
+        (save-restriction
+          (widen)
+          (if errbuf
+              (with-current-buffer errbuf
+                (setq buffer-read-only nil)
+                (erase-buffer)))
+          (with-current-buffer patchbuf
+            (erase-buffer))
+
+          (write-region nil nil tmpfile)
+
+          (setq our-auto-fix-args
+                (append our-auto-fix-args (list auto-fix-option tmpfile)))
+          (message "Calling auto-fix: %s %s" auto-fix-command our-auto-fix-args)
+          (if (zerop (apply #'process-file auto-fix-command nil errbuf nil our-auto-fix-args))
+              (progn
+                ;; There is no remote variant of ‘call-process-region’, but we
+                ;; can invoke diff locally, and the results should be the same.
+                (if (zerop (let ((local-copy (file-local-copy tmpfile)))
+                             (unwind-protect
+                                 (call-process-region
+                                  (point-min) (point-max) "diff" nil patchbuf
+                                  nil "-n" "-" (or local-copy tmpfile))
+                               (when local-copy (delete-file local-copy)))))
+                    (message "Buffer is already auto fixed")
+                  (auto-fix--apply-rcs-patch patchbuf)
+                  (message "Applied auto fix"))
+                (if errbuf (auto-fix--kill-error-buffer errbuf)))
+            (message "Could not apply auto fix")))
+
+      (kill-buffer patchbuf)
+      (delete-file tmpfile))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; Internal functions
+
+(defun auto-fix--make-temp-file ()
+  "Create temporary file at same directory for creating patch."
+  (let ((temporary-file-directory default-directory)
+        (prefix "auto-fix-")
+        (suffix (concat "." (file-name-extension buffer-file-name))))
+    (make-temp-file prefix nil suffix)))
+
+(defun auto-fix--delete-whole-line (&optional arg)
+  "Delete the current line without putting it in the `kill-ring'.
+Derived from function `kill-whole-line'.  ARG is defined as for that
+function."
+  (setq arg (or arg 1))
+  (if (and (> arg 0)
+           (eobp)
+           (save-excursion (forward-visible-line 0) (eobp)))
+      (signal 'end-of-buffer nil))
+  (if (and (< arg 0)
+           (bobp)
+           (save-excursion (end-of-visible-line) (bobp)))
+      (signal 'beginning-of-buffer nil))
+  (cond ((zerop arg)
+         (delete-region (progn (forward-visible-line 0) (point))
+                        (progn (end-of-visible-line) (point))))
+        ((< arg 0)
+         (delete-region (progn (end-of-visible-line) (point))
+                        (progn (forward-visible-line (1+ arg))
+                               (unless (bobp)
+                                 (backward-char))
+                               (point))))
+        (t
+         (delete-region (progn (forward-visible-line 0) (point))
+                        (progn (forward-visible-line arg) (point))))))
+
+(defun auto-fix--apply-rcs-patch (patch-buffer)
+  "Apply an RCS-formatted diff from PATCH-BUFFER to the current buffer."
+  (let ((target-buffer (current-buffer))
+        ;; Relative offset between buffer line numbers and line numbers
+        ;; in patch.
+        ;;
+        ;; Line numbers in the patch are based on the source file, so
+        ;; we have to keep an offset when making changes to the
+        ;; buffer.
+        ;;
+        ;; Appending lines decrements the offset (possibly making it
+        ;; negative), deleting lines increments it. This order
+        ;; simplifies the forward-line invocations.
+        (line-offset 0)
+        (column (current-column)))
+    (save-excursion
+      (with-current-buffer patch-buffer
+        (goto-char (point-min))
+        (while (not (eobp))
+          (unless (looking-at "^\\([ad]\\)\\([0-9]+\\) \\([0-9]+\\)")
+            (error "Invalid rcs patch or internal error in auto-fix--apply-rcs-patch"))
+          (forward-line)
+          (let ((action (match-string 1))
+                (from (string-to-number (match-string 2)))
+                (len  (string-to-number (match-string 3))))
+            (cond
+             ((equal action "a")
+              (let ((start (point)))
+                (forward-line len)
+                (let ((text (buffer-substring start (point))))
+                  (with-current-buffer target-buffer
+                    (cl-decf line-offset len)
+                    (goto-char (point-min))
+                    (forward-line (- from len line-offset))
+                    (insert text)))))
+             ((equal action "d")
+              (with-current-buffer target-buffer
+                (goto-char (point-min))
+                (forward-line (1- (- from line-offset)))
+                 (cl-incf line-offset len)
+                (auto-fix--delete-whole-line len)))
+             (t
+              (error "Invalid rcs patch or internal error in auto-fix--apply-rcs-patch")))))))
+    (move-to-column column)))
+
+(defun auto-fix--kill-error-buffer (errbuf)
+  "Kill ERRBUF buffer."
+  (let ((win (get-buffer-window errbuf)))
+    (if win
+        (quit-window t win)
+      (kill-buffer errbuf))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
